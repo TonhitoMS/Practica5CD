ee4c9264e555e9568dd30592f6d478f4e5cfd3c1
TonhitoMS <antonio.moscoso.sanchez@gmail.com>
2021-11-19 12:06:07 +0100

    Nova clase peer

---
+++ aplicacion/Peer.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aplicacion;
+
+/**
+ *
+ * @author antonio
+ */
+public class Peer {
+    private String nombre;
+    private String ip;
+    private int puerto;
+
+    public Peer(String nombre, String ip, int puerto) {
+        this.nombre = nombre;
+        this.ip = ip;
+        this.puerto = puerto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public int getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(int puerto) {
+        this.puerto = puerto;
+    }
+    
+    
+    
+}
